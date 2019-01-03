f120060dd8a3c0ce4b5e5a2ebae45ca8f529e827
DenisCompany <30878230+DenisCompany@users.noreply.github.com>
2019-01-03 04:25:12 +0400

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 float version = 0.2;
 float oneNumb, twoNumb;
 
-bool active;
+int active=1;
 int main(){
     char value;
     printf("Hello, I calc vesion %.2f and My creater is Denis Mustafin.\n ", version);
@@ -14,24 +14,24 @@ int main(){
         scanf("%f", &oneNumb);
         printf("Input your second number, pls->");
         scanf("%f", &twoNumb);
-        printf("What do you want doing now? *, /, +, -.  -->");
+        printf("What do you want doing now? *, /, +, -. \ndor you can exit insert | -->");
         scanf("%s", &value);
         switch(value) {
-        case '+': printf("%f+%f=%f\n", oneNumb, twoNumb, oneNumb+twoNumb);
+        case '+': printf("%.f+%.f=%.f\n", oneNumb, twoNumb, oneNumb+twoNumb);
         break;
-         case '-': printf("%f-%f=%f\n", oneNumb, twoNumb, oneNumb-twoNumb);
+         case '-': printf("%.f-%.f=%.f\n", oneNumb, twoNumb, oneNumb-twoNumb);
         break;
-         case '*': printf("%f*%f=%f\n", oneNumb, twoNumb, oneNumb*twoNumb);
+         case '*': printf("%.f*%.f=%.f\n", oneNumb, twoNumb, oneNumb*twoNumb);
         break;
-         case '/': printf("%f/%f=%f\n", oneNumb, twoNumb, oneNumb/twoNumb);
+         case '/': printf("%.f/%.f=%.f\n", oneNumb, twoNumb, oneNumb/twoNumb);
         break;
         case '|' : printf("Exit, Goodbye my friend!");
 
-            active = FALSE;
+            active = 0;
             break;
     default:
-    printf("Nothing  =)\n");
+        printf("Nothing  =)\n");
         }
 
-    } while(active==TRUE);
+    } while(active==1);
 };
